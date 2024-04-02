68bf24752841dd03bcfdcbb5a2949e84db2d4a61
Akriti <akritikohli05@gmail.com>
2024-04-02 14:17:35 +0530

    fixed an error in moviesuggestion

--- src/components/AiMovieSuggestions.js
+++ src/components/AiMovieSuggestions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import MovieList from "./MovieList"
 import { useSelector } from 'react-redux';
 
@@ -16,16 +15,4 @@ const AiMovieSuggestions = () => {
 
 export default AiMovieSuggestions;
 
-=======
-import React from 'react'
 
-const aiMovieSuggestions = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default aiMovieSuggestions
->>>>>>> 5f4541f27e62b1ab4b4877d07eb074ed7887041a
