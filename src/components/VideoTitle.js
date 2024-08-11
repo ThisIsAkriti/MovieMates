d6ef443f06f7e4b3e1d4c0d654de92b8525fce39
Akriti <akritikohli05@gmail.com>
2024-08-11 13:43:27 +0530

    fixed css

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -8,17 +8,15 @@ const VideoTitle = ({title , overview}) => {
       <h1 className=' xl:text-4xl lg:text-3xl text-xl font-bold mb-4 xl:mb-0'>{title}</h1>
       <p className='hidden xl:inline-block py-6 text-lg w-1/3'>{overview}</p>
       <div className='flex '>
-        <button className='flex items-center bg-gray-100 font-semibold text-black xl:px-4 px-2 py-2 
-          border-gray-200 border-2 rounded-lg mr-4 
-          hover:opacity-80 '>
+        <button className='flex items-center bg-gray-100 font-semibold text-black xl:px-4 px-3 py-2 border-purple-600 border rounded-lg mr-4 hover:bg-purple-400 hover:bg-opacity-80 hover:shadow-md hover:shadow-purple-800 hover:border-none'>
           <img className='mr-1 xl:size-6 size-4' src={play_icon} alt="play_icon" />
           Play
         </button>
         
-        <button className='flex items-center bg-gray-900 font-semibold text-white xl:px-4 px-2 py-2
-          border-gray-900   
+        <button className='flex items-center bg-purple-900 font-semibold text-white xl:px-4 px-2 py-2
+          border-purple-900   
           border-2 rounded-lg mr-4
-          bg-opacity-75 hover:bg-black hover:bg-opacity-90'>
+          bg-opacity-75 hover:bg-purple-800 hover:bg-opacity-90'>
           <img className='mr-1 xl:size-6 size-5' src={icon_info2} alt="info-icon" />
           More Info
         </button>
