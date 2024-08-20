da18886775548463948bf012086cd22706df864c
Akriti <akritikohli05@gmail.com>
2024-08-20 19:42:37 +0530

    changed regex

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,7 +2,7 @@ export const checkValidData = (email, password) => {
     
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
    
-    const isPasswordValid = /^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\D*\d)(?=[^!#%]*[!#%])[A-Za-z0-9!#%]{8,32}$/.test(password);
+    const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(password);
 
     if(!isEmailValid) return "Email is not valid";
     if(!isPasswordValid) return "Password is not valid";
